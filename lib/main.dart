45f94d9771b121974cfad98228c4a0ec6a63e6e6
GukGwonGae <gukgwongae@gmail.com>
2025-12-09 12:32:12 +0900

    feat: bottomnavbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:cute_story_closed_sns_app/core/config/firebase_options.dart';
 import 'package:cute_story_closed_sns_app/core/theme/app_theme.dart';
+import 'package:cute_story_closed_sns_app/presentation/pages/add/add_page.dart';
+import 'package:cute_story_closed_sns_app/presentation/pages/bottomnavbar/bottomnavbar.dart';
 import 'package:cute_story_closed_sns_app/presentation/pages/comments/comments_page.dart';
 import 'package:cute_story_closed_sns_app/presentation/pages/my_page/my_page.dart';
 import 'package:cute_story_closed_sns_app/presentation/pages/post_list/post_list_page.dart';
@@ -14,9 +16,17 @@ void main() async {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int selectedIndex = 0;
+
+  final pages = [PostListPage(), AddPage(), MyPage()];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,7 +37,15 @@ class MyApp extends StatelessWidget {
       // 첫 화면은 PostListPage로 설정
       // home: MyPage(),
       // home: MyPage(),
-      home: PostListPage(),
+      // home: PostListPage(),
+      // 바텀 내브 적용 후 (bottomnavbar.dart 파일 밑에 설명 읽고 실행)
+      home: Scaffold(
+        body: pages[selectedIndex],
+        bottomNavigationBar: Bottomnavbar(
+          currentIndex: selectedIndex,
+          onTap: (index) => setState(() => selectedIndex = index),
+        ),
+      ),
     );
   }
 }
